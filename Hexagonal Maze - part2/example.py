a5ce1dbc73a951a12aac2a1ef16cc625bb208b73
DevIA05 <dia05@polenumerique.re>
2023-01-03 14:18:42 +0400

    brouillon

--- Hexagonal Maze - part2/example.py
+++ Hexagonal Maze - part2/example.py
@@ -1,93 +1,72 @@
-
-# import sys
-# import math
-
-# class node:
-#     def __init__(self, valeur, position, voisinage):
-#         self.val       = valeur
-#         self.pos       = position
-#         self.voisinage = voisinage 
-
-
-# node_list  = []
-# maze_list  = []
-
-# # Auto-generated code below aims at helping you parse
-# # the standard input according to the problem statement.
-# w, h = [int(i) for i in input().split()]
-# #print(w, file=sys.stderr, flush=True)
-# for ligne in range(h):
-#     row = input()
-#     ligne_list = []
-#     for index in range(0, len(row)):
-#         cell = row[index]
-#         ligne_list.append(cell)
-#     maze_list.append(ligne_list)
-
-
-
-# for l in range(0, len(maze_list)):
-#     for i in range(0, len(maze_list[l])):
-#         cell = maze_list[l][i]
-#         if(cell != "#"):
-#             pass
-
-# def getNeighbor(l, i, maze_list):
-#     list_neighbor = []
-#     # voisinage en haut 
-#     if(l > 0):
-#         list_neighbor.append(maze_list[l-1][i])
-#     # voisinage en bas 
-#     if(l < len(maze_list)-1):
-#         list_neighbor.append(maze_list[l+1][i])
-#     # à gauche
-#     if(i > 0):
-#         list_neighbor.append(maze_list[l][i-1])
-#     # à droite
-#     if(i < len(maze_list[l])-1):
-#         list_neighbor.append(maze_list[l][i+1])
-#         list_neighbor.append(maze_list[l+1][i+1])
-#     return list_neighbor
-        
-
-
 import sys
 import math
 
-node_list  = []
-maze_list  = []
-#padding_ch = "#"
-
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-w, h = [int(i) for i in input().split()]
-for ligne in range(h):
-    row = input()
-    print(row, file=sys.stderr, flush=True)
-    ligne_list = []
-    for index in range(0, len(row)):
-        cell = row[index]
-        ligne_list.append(cell)
-    maze_list.append(ligne_list)
+def maze_list():
+    maze = []
+    w, h = [int(i) for i in input().split()]
+    for ligne in range(h):
+        row = input()
+        ligne_list = []
+        for index in range(0, len(row)):
+            cell = row[index]
+            ligne_list.append(cell)
+        maze.append(ligne_list)
+    return maze
+
+maze = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'], 
+        ['#', 'S', '.', '_', '_', '_', '.', '#', '#', '#'], 
+        ['#', '#', '#', '#', '#', '#', '#', '.', 'E', '#'], 
+        ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
 
 
-def getNeighbor(coord, maze_list):
+def bfs(maze, start, end=None):  # Breadth-First Search
+    queue = [start]                                 # enqueue the source node  
+    visited = []                                    # list of nodes visited
+    predecessor = {}                       
+    while len(queue) >  0:
+        node_coord = queue.pop(0)                   # remove the node from the start of the queue to process it   
+        visited.append(node_coord)                  
+        for n in getNeighbor(node_coord, maze):     # queue all its unexplored neighbors (at the end);
+            if n not in predecessor.keys():
+                queue.append(n)
+                predecessor[n] = node_coord
+    return visited, predecessor
+    
+def getNeighbor(coord, maze):
+    width, heigth = len(maze[0]), len(maze)
     x, y = coord
-    list_neighbor = [(x+1, y), (x-1, y), 
-                     (x, y+1), (x, y-1)]
+    potential_neighbor = [(x+1, y), (x-1, y),     # right, left 
+                          (x, y+1), (x, y-1),     # top, down
+                          (x-1, y-1), (x-1, y+1), # top diagonal left, right
+                          (x+1, y-1), (x+1, y+1)] # bottom diagonal left, right
+    neighbor = [(l, c) for (l, c) in potential_neighbor if  0<=l<heigth and 0<=c<width and maze[l][c] != "#" ] 
+    return neighbor
 
-    #return list_neighbor
-        
+def theWayTo(end, start, p):
+    route = [end]
+    while end != start:
+        end = p[end]
+        route.append(end)
+    return route[::-1]
+
+v, p = bfs(maze, (1,1))
+r = theWayTo(end=(2,8), start=(1,1), p=p)
+print(" ")
+print(" ")
+print("visited")
+print(v)
+print(" ")
+print("predecessor")
+print(p)
+print(" ")
+print(" ")
+print("route")
+print(r)
 
 
 
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-
-
-
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
 

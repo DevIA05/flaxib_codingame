77ed6d5f4b5fb87843896efb1f59745789337e7d
DevIA05 <dia05@polenumerique.re>
2023-01-11 14:43:31 +0400

    save

--- Hexagonal_Maze_part2/script/main.py
+++ Hexagonal_Maze_part2/script/main.py
@@ -1,40 +1,32 @@
-# sets the working directory at the level of the file's parent
-import sys
-from pathlib import Path
-folder = Path(__file__).parent
-sys.path.insert(0, str(folder))
+# sets the working directory at the level of the file's parent ######
+import sys                                                          #
+from pathlib import Path                                            #
+folder = Path(__file__).parent                                      #
+sys.path.insert(0, str(folder))                                     #
+#####################################################################
 
 from function import *
 import copy
 
-maze_str = '''##########
-#S.___.###
-#######.E#
-##########'''.replace('\n', "")
-resp = "R R DR R"
+# pick up the labyrinth to go through it
+file = "maze11.txt"
 
-# Shape of the maze
-width  = 10 
-height = 4  
+#####################################################################
+########################## MAIN #####################################
+#####################################################################
 # Transform the str maze into a list adapted to the hexagonal format
 # and also returns a list of keys, gate, entry point and exit point
-maze, l = mazeStrToList(maze_str = maze_str,          
-                        width=width, heigth=height)
-#Remove keyless doors and turn them into a wall
-mazewithoutDoor = door_wall(maze = maze, letter = l)
-atw = stepByStep(maze = mazewithoutDoor, letter = l)
+maze, letter, resp  = getMaze(file=file)
+original_maze = copy.deepcopy(maze) # comment in coding game
+door_wall(maze = maze, letter = letter)
+atw = stepByStep(maze = maze, letter = letter)
 d = coordToLetter(atw)
 myResp = response(direction=d)
 ca = checkAnswer(resp=resp, myResp=myResp)
-mazeWithRecord = recordMouvement(maze=maze, allTheWay=atw)
-mazeWithRecord_str = printMaze(mazeWithRecord)
 
-
-print("="*25 + " MAZE " + "="*25); 
-print(printMaze(maze)); print(" ")
-#print(mwdPrint); print("\n")
-print("="*15 + " MAZE WITH RECORD " + "="*15); print(mazeWithRecord_str)
-print("\n")
+#####################################################################
+######################### PRINT RESULT ##############################
+#####################################################################
 print("="*25 + " RESULT " + "="*25); print("")
 print("Le parcours"); 
 for i in range(0, len(d)): print(f"{i+1}: {d[i]}", end="; ")
@@ -43,11 +35,11 @@ print("Bonne réponse"); print(resp);
 print("")
 print("Réussite: ", end=""); print(ca); print("")
 
-# print ("="*25 + "BROUILLON" + "="*25)
-# mazeWithoutDoor = door_wall(maze= maze, letter= l)
-# mwd_str = printMaze(mazeWithoutDoor)
-# print(mwd_str)
-with open('Hexagonal_Maze_part2/maze/maze10.txt','w+') as f:
-    f.write(printMaze(maze))
-with open('Hexagonal_Maze_part2/maze/Record_maze10.txt','w+') as f:
+#####################################################################
+######################## WRITTING RECORD ############################
+#####################################################################
+print("="*25 + " WRITTING RECORD " + "="*25); print("")
+mazeWithRecord = recordMouvement(maze=original_maze, allTheWay=atw)
+mazeWithRecord_str = printMaze(mazeWithRecord)
+with open('Hexagonal_Maze_part2/maze/Record_'+ file + '.txt','w+') as f:
     f.write(mazeWithRecord_str)
